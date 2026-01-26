86093ab57d3f74b8a49b1e6a4a3fbd7b30bc818f
Alokafernando <buddhikaaloka2005@gmail.com>
2026-01-26 15:28:24 +0530

    implement user registration functionality with Firebase

---
+++ services/authService.ts
@@ -0,0 +1,21 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { auth, db } from "./firebase"
+import { doc, setDoc } from "firebase/firestore"
+
+export const registerUser = async (fullname: string, email: string, password: string, confirmPassword: string) => {
+
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        await updateProfile(userCredentials.user, { displayName: fullname })
+        await setDoc(doc(db, "users", userCredentials.user.uid), {
+            name: fullname,
+            role: "USER",
+            email,
+            createAt: new Date()
+        })
+        return userCredentials.user
+    } catch (error) {
+        console.error("Error registering user:", error)
+    }
+
+}
