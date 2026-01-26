bee2241c8416c132488f169cfdbee03ded006b71
Alokafernando <buddhikaaloka2005@gmail.com>
2026-01-26 15:18:36 +0530

    add firebase configuration and async storage dependency

---
+++ services/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+// @ts-ignore
+import { initializeAuth, getReactNativePersistence } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHGDau31PzW1mvGxOsokiXxiqSpEFk_Z8",
+  authDomain: "burnfree-ai-88b29.firebaseapp.com",
+  projectId: "burnfree-ai-88b29",
+  storageBucket: "burnfree-ai-88b29.firebasestorage.app",
+  messagingSenderId: "37989712528",
+  appId: "1:37989712528:web:a5e8eae802e0d392de8e15"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+})
+
+export const db = getFirestore(app)
