0727bfb4bc296207964d34fb80be36561e90d3ea
Alokafernando <buddhikaaloka2005@gmail.com>
2026-01-27 23:33:44 +0530

    update firebase service

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,8 +1,31 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-// @ts-ignore
-import { initializeAuth, getReactNativePersistence } from "firebase/auth"
+// import { initializeApp } from "firebase/app"
+// import { getFirestore } from "firebase/firestore"
+// import AsyncStorage from "@react-native-async-storage/async-storage"
+// // @ts-ignore
+// import { initializeAuth, getReactNativePersistence } from "firebase/auth"
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAHGDau31PzW1mvGxOsokiXxiqSpEFk_Z8",
+//   authDomain: "burnfree-ai-88b29.firebaseapp.com",
+//   projectId: "burnfree-ai-88b29",
+//   storageBucket: "burnfree-ai-88b29.firebasestorage.app",
+//   messagingSenderId: "37989712528",
+//   appId: "1:37989712528:web:a5e8eae802e0d392de8e15"
+// }
+
+// const app = initializeApp(firebaseConfig)
+
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage)
+// })
+
+// export const db = getFirestore(app)
+
+
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHGDau31PzW1mvGxOsokiXxiqSpEFk_Z8",
@@ -10,13 +33,10 @@ const firebaseConfig = {
   projectId: "burnfree-ai-88b29",
   storageBucket: "burnfree-ai-88b29.firebasestorage.app",
   messagingSenderId: "37989712528",
-  appId: "1:37989712528:web:a5e8eae802e0d392de8e15"
-}
+  appId: "1:37989712528:web:a5e8eae802e0d392de8e15",
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-})
-
-export const db = getFirestore(app)
+export const auth = getAuth(app); 
+export const db = getFirestore(app);
